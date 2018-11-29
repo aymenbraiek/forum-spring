484915612780b554b469013c82e1b58b754e6c3a
aymen braiek <aymen.braiek@esprit.tn>
2018-11-29 21:31:00 +0100

    initial project version

---
+++ src/main/java/com/org/esprit/forumesprit/ForumEspritApplication.java
@@ -0,0 +1,12 @@
+package com.org.esprit.forumesprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumEspritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumEspritApplication.class, args);
+	}
+}
